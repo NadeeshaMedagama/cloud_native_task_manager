b20cb3b787d1bd7a5e697c7b7ab1cb52f5570747
NadeeshaMedagama <nadeeshamedagama@gmail.com>
2026-03-23 08:39:10 +0530

    login request dto file added

---
+++ common/src/main/java/com/taskmanager/common/dto/LoginRequestDTO.java
@@ -0,0 +1,20 @@
+package com.taskmanager.common.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequestDTO {
+    @NotBlank(message = "Username is required")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+}
+
