7a48f9245ec92184666907381cff1ebb2751dd31
NadeeshaMedagama <nadeeshamedagama@gmail.com>
2026-03-23 08:37:51 +0530

    user controller file added

---
+++ user-service/src/main/java/com/taskmanager/userservice/web/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.taskmanager.userservice.web.controller;
+
+import com.taskmanager.userservice.application.service.UserService;
+import com.taskmanager.common.dto.UserDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<UserDTO>> getAllUsers(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+        return ResponseEntity.ok(userService.getAllUsers(page, size));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> getUserById(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+}
+
