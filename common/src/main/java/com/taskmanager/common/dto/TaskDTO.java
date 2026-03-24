63669db76b1f908d5ee3bbd0058407c70e841b7e
NadeeshaMedagama <nadeeshamedagama@gmail.com>
2026-03-24 10:14:47 +0530

    task dto file added

---
+++ common/src/main/java/com/taskmanager/common/dto/TaskDTO.java
@@ -0,0 +1,22 @@
+package com.taskmanager.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TaskDTO {
+    private Long id;
+    private String title;
+    private String description;
+    private String status;
+    private String priority;
+    private String dueDate;
+    private Long ownerId;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
+
